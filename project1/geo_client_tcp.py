f4f930c4b7ae1385f603556c247c2d13b7606fa2
Mason <donnma01@luther.edu>
2018-09-05 21:40:27 -0500

    restarting computer to restart server

--- project1/geo_client_tcp.py
+++ project1/geo_client_tcp.py
@@ -3,16 +3,29 @@ GEO TCP Client
 '''
 #!/usr/bin/env python3
 
-from socket import socket, AF_INET, SOCK_STREAM
+import socket
+import sys
 
 HOST = 'localhost'
 PORT = 4300
 
 
 def client():
-    '''Main client loop'''
-    # TODO: Implement client-side tasks
-    pass
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+        print('Connected to {}:{}'.format(HOST, PORT))
+        ucountry = input("Enter a country or BYE to quit\n")
+        while not(ucountry == "BYE"):
+            s.sendall(ucountry.encode())
+            capital = s.recv(1024).decode()
+            print("HERE1")
+            if capital == "NA":
+                print("-There is no such country")
+            else:
+                print('+{}'.format(capital))
+            ucountry = input("Enter another country to try again or BYE to quit\n")
+        s.close()
+        print('Connection closed')
 
 def main():
     '''Main function'''
